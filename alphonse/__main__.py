c9e28782c1be815f2e8d96f98019de881f28a908
nhomble <nhomble@terpmail.umd.edu>
2022-08-21 08:54:39 -0400

    never die

--- alphonse/__main__.py
+++ alphonse/__main__.py
@@ -26,10 +26,17 @@ def message():
         "Kyle cannot fail"
     ])
 
+def keep_alive(groupme, group_id, user_id):
+    group = groupme.group.get(group_id)
+
+    if all([m.user_id != user_id for m in group.members]):
+        group.memberships.add("never die", user_id=user_id)
 
 if __name__ == '__main__':
     bot_id = os.getenv("GROUPME_BOT")
     api_key = os.getenv("GROUPME_KEY")
+    group_id = os.getenv("GROUPME_GROUP_ID")
+    user_id = os.getenv("GROUPME_ALIVE")
 
     if bot_id is None:
         print("Missing GROUPME_BOT")
@@ -39,6 +46,14 @@ if __name__ == '__main__':
         print("Missing GROUPME_KEY")
         sys.exit(1)
 
+    if group_id is None:
+        print("Missing GROUPME_GROUP_ID")
+        sys.exit(1)
+
+    if user_id is None:
+        print("Missing GROUPME_ALIVE")
+        sys.exit(1)
+
     groupme = Client.from_token(api_key)
 
     if should_bark():
@@ -46,3 +61,6 @@ if __name__ == '__main__':
         groupme.bots.post(bot_id, message())
     else:
         print("Skipping execution")
+
+    keep_alive(groupme, group_id, user_id)
+
